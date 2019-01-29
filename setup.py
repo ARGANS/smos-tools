608043fa565b204281419e8dc92011e248d52eff
Simon Watts <swatts@argans.co.uk>
2019-01-29 17:34:59 +0000

    Format ready to be a python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name=smos_tools,
+        version='0.1',
+        description='Some open source tools for SMOS',
+        url='https://www.argans.co.uk',
+        author='ARGANS',
+        author_email='smos@argans.co.uk',
+        license='MIT',
+        packages=['smos_tools'],
+        zip_safe=False)
