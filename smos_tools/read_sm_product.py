344eacc3092499645b7a92b43202ecfa42c331ba
rsdavies <rdavies@argans.co.uk>
2019-03-12 12:39:15 +0000

    removes pandas setting with copy warning by taking copy of dframes. Also brings all commas into pep8 standard

--- smos_tools/read_sm_product.py
+++ smos_tools/read_sm_product.py
@@ -9,97 +9,98 @@ schema_path = 'schema/DBL_SM_XXXX_MIR_SMUDP2_0400.binXschema.xml'
 data_path = 'data/SM_TEST_MIR_SMUDP2_20150721T102717_20150721T112036_650_001_9.DBL'
 
 # Primary array datatype, repeated n_grid_points times, Grid_Point_Data_Type
-datatype = [('Grid_Point_ID',np.uint32),
-            ('Latitude',np.float32),
-            ('Longitude',np.float32),
-            ('Altitude',np.float32),
+datatype = [('Grid_Point_ID', np.uint32),
+            ('Latitude', np.float32),
+            ('Longitude', np.float32),
+            ('Altitude', np.float32),
             # UTC_Type
             ('Mean_Acq_Time', [
-                ('Days',np.int32),
-                ('Seconds',np.uint32),
-                ('Microseconds',np.uint32)
+                ('Days', np.int32),
+                ('Seconds', np.uint32),
+                ('Microseconds', np.uint32)
             ]),
             # Retrieval_Results_Data_Type
             ('Retrieval_Results_Data', [
-                ('Soil_Moisture',np.float32),
-                ('Soil_Moisture_DQX',np.float32),
-                ('Optical_Thickness_Nad',np.float32),
-                ('Optical_Thickness_Nad_DQX',np.float32),
-                ('Surface_Temperature',np.float32),
-                ('Surface_Temperature_DQX',np.float32),
-                ('TTH',np.float32),
-                ('TTH_DQX',np.float32),
-                ('RTT',np.float32),
-                ('RTT_DQX',np.float32),
-                ('Scattering_Albedo_H',np.float32),
-                ('Scattering_Albedo_H_DQX',np.float32),
-                ('DIFF_Albedos',np.float32),
-                ('DIFF_Albedos_DQX',np.float32),
-                ('Roughness_Param',np.float32),
-                ('Roughness_Param_DQX',np.float32),
-                ('Dielect_Const_MD_RE',np.float32),
-                ('Dielect_Const_MD_RE_DQX',np.float32),
-                ('Dielect_Const_MD_IM',np.float32),
-                ('Dielect_Const_MD_IM_DQX',np.float32),
-                ('Dielect_Const_Non_MD_RE',np.float32),
-                ('Dielect_Const_Non_MD_RE_DQX',np.float32),
-                ('Dielect_Const_Non_MD_IM',np.float32),
-                ('Dielect_Const_Non_MD_IM_DQX',np.float32),
-                ('TB_ASL_Theta_B_H',np.float32),
-                ('TB_ASL_Theta_B_H_DQX',np.float32),
-                ('TB_ASL_Theta_B_V',np.float32),
-                ('TB_ASL_Theta_B_V_DQX',np.float32),
-                ('TB_TOA_Theta_B_H',np.float32),
-                ('TB_TOA_Theta_B_H_DQX',np.float32),
-                ('TB_TOA_Theta_B_V',np.float32),
-                ('TB_TOA_Theta_B_V_DQX',np.float32)
+                ('Soil_Moisture', np.float32),
+                ('Soil_Moisture_DQX', np.float32),
+                ('Optical_Thickness_Nad', np.float32),
+                ('Optical_Thickness_Nad_DQX', np.float32),
+                ('Surface_Temperature', np.float32),
+                ('Surface_Temperature_DQX', np.float32),
+                ('TTH', np.float32),
+                ('TTH_DQX', np.float32),
+                ('RTT', np.float32),
+                ('RTT_DQX', np.float32),
+                ('Scattering_Albedo_H', np.float32),
+                ('Scattering_Albedo_H_DQX', np.float32),
+                ('DIFF_Albedos', np.float32),
+                ('DIFF_Albedos_DQX', np.float32),
+                ('Roughness_Param', np.float32),
+                ('Roughness_Param_DQX', np.float32),
+                ('Dielect_Const_MD_RE', np.float32),
+                ('Dielect_Const_MD_RE_DQX', np.float32),
+                ('Dielect_Const_MD_IM', np.float32),
+                ('Dielect_Const_MD_IM_DQX', np.float32),
+                ('Dielect_Const_Non_MD_RE', np.float32),
+                ('Dielect_Const_Non_MD_RE_DQX', np.float32),
+                ('Dielect_Const_Non_MD_IM', np.float32),
+                ('Dielect_Const_Non_MD_IM_DQX', np.float32),
+                ('TB_ASL_Theta_B_H', np.float32),
+                ('TB_ASL_Theta_B_H_DQX', np.float32),
+                ('TB_ASL_Theta_B_V', np.float32),
+                ('TB_ASL_Theta_B_V_DQX', np.float32),
+                ('TB_TOA_Theta_B_H', np.float32),
+                ('TB_TOA_Theta_B_H_DQX', np.float32),
+                ('TB_TOA_Theta_B_V', np.float32),
+                ('TB_TOA_Theta_B_V_DQX', np.float32)
             ]),
             # Confidence_Descriptors_Data_Type
             ('Confidence_Descriptors_Data', [
-                ('Confidence_Flags',np.uint16),
-                ('GQX',np.uint8),
-                ('Chi_2',np.uint8),
-                ('Chi_2_P',np.uint8),
-                ('N_Wild',np.uint16),
-                ('M_AVA0',np.uint16),
-                ('M_AVA',np.uint16),
-                ('AFP',np.float32),
-                ('N_AF_FOV',np.uint16),
-                ('N_Sun_Tails',np.uint16),
-                ('N_Sun_Glint_Area',np.uint16),
-                ('N_Sun_FOV',np.uint16),
-                ('N_RFI_Mitigations',np.uint16),
-                ('N_Strong_RFI',np.uint16),
-                ('N_Point_Source_RFI',np.uint16),
-                ('N_Tails_Point_Source_RFI',np.uint16),
-                ('N_Software_Error',np.uint16),
-                ('N_Instrument_Error',np.uint16),
-                ('N_ADF_Error',np.uint16),
-                ('N_Calibration_Error',np.uint16),
-                ('N_X_Band',np.uint16)
+                ('Confidence_Flags', np.uint16),
+                ('GQX', np.uint8),
+                ('Chi_2', np.uint8),
+                ('Chi_2_P', np.uint8),
+                ('N_Wild', np.uint16),
+                ('M_AVA0', np.uint16),
+                ('M_AVA', np.uint16),
+                ('AFP', np.float32),
+                ('N_AF_FOV', np.uint16),
+                ('N_Sun_Tails', np.uint16),
+                ('N_Sun_Glint_Area', np.uint16),
+                ('N_Sun_FOV', np.uint16),
+                ('N_RFI_Mitigations', np.uint16),
+                ('N_Strong_RFI', np.uint16),
+                ('N_Point_Source_RFI', np.uint16),
+                ('N_Tails_Point_Source_RFI', np.uint16),
+                ('N_Software_Error', np.uint16),
+                ('N_Instrument_Error', np.uint16),
+                ('N_ADF_Error', np.uint16),
+                ('N_Calibration_Error', np.uint16),
+                ('N_X_Band', np.uint16)
             ]),
             # Science_Descriptors_Data_Type
             ('Science_Descriptors_Data', [
-                ('Science_Flags',np.uint32),
-                ('N_Sky',np.uint16)
+                ('Science_Flags', np.uint32),
+                ('N_Sky', np.uint16)
             ]),
             # Processing_Descriptors_Data_Type
             ('Processing_Descriptors_Data', [
-                ('Processing_Flags',np.uint16),
-                ('S_Tree_1',np.uint8),
-                ('S_Tree_2',np.uint8)
+                ('Processing_Flags', np.uint16),
+                ('S_Tree_1', np.uint8),
+                ('S_Tree_2', np.uint8)
             ]),
             # DGG_Current_Data_Type
             ('DGG_Current_Data', [
-                ('DGG_Current_Flags',np.uint8),
-                ('Tau_Cur_DQX',np.float32),
-                ('HR_Cur_DQX',np.float32),
-                ('N_RFI_X',np.uint16),
-                ('N_RFI_Y',np.uint16),
-                ('RFI_Prob',np.uint8)
+                ('DGG_Current_Flags', np.uint8),
+                ('Tau_Cur_DQX', np.float32),
+                ('HR_Cur_DQX', np.float32),
+                ('N_RFI_X', np.uint16),
+                ('N_RFI_Y', np.uint16),
+                ('RFI_Prob', np.uint8)
             ]),
-            ('X_Swath',np.uint16)
-        ]
+            ('X_Swath', np.uint16)
+            ]
+
 
 # Read in a SMOS SM data product, and return a numpy structured array.
 def read_sm_product(filepath):
@@ -114,6 +115,7 @@ def read_sm_product(filepath):
         print('Done')
 
     return data
+
 
 # Take a numpy structured SM format array and extract just the Soil Moisture information
 # into a pandas dataframe
@@ -135,6 +137,7 @@ def extract_sm(data):
 
     return soil_moisture
 
+
 # Plot any SM values from a dataframe that aren't NaN, with their lat/lon position
 def plot_sm(data_frame):
     # Assume a roughly continuous data region for now, just plot all datapoints that aren't -999.
@@ -144,6 +147,7 @@ def plot_sm(data_frame):
 
     axes = data_frame.plot.scatter('Grid_Point_ID', 'Soil_Moisture')
     plt.show()
+
 
 # Plot difference between 2 dataframes containing soil moisture
 def evaluate_sm_diff(smdf1, smdf2):
@@ -168,15 +172,16 @@ def evaluate_sm_diff(smdf1, smdf2):
     outer = pd.merge(frame1, frame2, how='outer', on=['Days', 'Seconds', 'Microseconds', 'Grid_Point_ID'],
                      indicator=True)
     # Get records in 1 but not 2
-    leftonly = outer[outer['_merge'] == 'left_only']
-    leftonly.rename(columns={'Latitude_x': 'Latitude', 'Longitude_x': 'Longitude', 'Soil_Moisture_x': 'Soil_Moisture'}, inplace=True)
+    leftonly = outer[outer['_merge'] == 'left_only'].copy()
+    leftonly.rename(columns={'Latitude_x': 'Latitude', 'Longitude_x': 'Longitude', 'Soil_Moisture_x': 'Soil_Moisture'},
+                               inplace=True)
     leftonly.drop('Latitude_y', axis=1, inplace=True)
     leftonly.drop('Longitude_y', axis=1, inplace=True)
     leftonly.drop('Soil_Moisture_y', axis=1, inplace=True)
     leftonly.drop('_merge', axis=1, inplace=True)
 
     # Get records in 2 but not 1
-    rightonly = outer[outer['_merge'] == 'right_only']
+    rightonly = outer[outer['_merge'] == 'right_only'].copy()
     rightonly.rename(columns={'Latitude_x': 'Latitude', 'Longitude_x': 'Longitude', 'Soil_Moisture_y': 'Soil_Moisture'}, inplace=True)
     rightonly.drop('Latitude_y', axis=1, inplace=True)
     rightonly.drop('Longitude_y', axis=1, inplace=True)
