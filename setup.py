1f1b6c7f6ec4e7a96de9199ae225fbcfd78c646f
Rhiannon Davies <rdavies@argans.co.uk>
2019-05-13 15:51:54 +0100

    update setup.py to handle command line interface in bin

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
-setup(name=smos_tools,
-        version='0.1',
-        description='Some open source tools for SMOS',
-        url='https://www.argans.co.uk',
-        author='ARGANS',
-        author_email='smos@argans.co.uk',
-        license='MIT',
-        packages=['smos_tools'],
-        zip_safe=False)
+setup(name='smos_tools',
+      version='0.1',
+      description='Some open source tools for SMOS',
+      url='https://www.argans.co.uk',
+      author='ARGANS',
+      author_email='smos@argans.co.uk',
+      license='MIT',
+      packages=['smos_tools', 'smos_tools.data', 'smos_tools.data_types',
+                'smos_tools.logger', 'smos_tools.schema'],
+      package_dir={'smos_tools.data': 'smos_tools/data'},
+      package_data={'smos_tools.data':
+                    ['smos_tools/data/SM_TEST_MIR_SMUDP2_20150721T102717_20150721T112036_650_001_9.DBL',
+                     'smos_tools/data/SM_TEST_MIR_SMUDP2_20150721T102717_20150721T112036_650_001_9.HDR']
+                    },
+      include_package_data=True,
+      scripts=['bin/read_sm_product'],
+      zip_safe=False,
+      )
