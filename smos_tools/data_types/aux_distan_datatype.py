4aaa982da4f6805f5eed6159396e0797b0f60592
Simon Watts <swatts@argans.co.uk>
2019-09-04 18:06:08 +0100

    Unpack AUX_DISTAN flags, and report sea/land

--- smos_tools/data_types/aux_distan_datatype.py
+++ smos_tools/data_types/aux_distan_datatype.py
@@ -13,3 +13,13 @@ datatype = [('Grid_Point_ID', np.uint32),
             ('Tg_resol_max_ocean', np.float32),
             ('Sea_Ice_Mask', np.uint16)
             ]
+
+flag_datatype = [('Fg_Land_Sea_Coast1_tot', np.bool),
+                 ('Fg_Land_Sea_Coast2_tot', np.bool),
+                 ('Blank1', np.bool),
+                 ('Blank2', np.bool),
+                 ('Blank3', np.bool),
+                 ('Blank4', np.bool),
+                 ('Blank5', np.bool),
+                 ('Blank6', np.bool)
+                ]
--- smos_tools/read_aux_distan_product.py
+++ smos_tools/read_aux_distan_product.py
@@ -5,7 +5,7 @@ import pandas as pd
 import logging
 import logging.config
 import os
-from smos_tools.data_types.aux_distan_datatype import datatype
+from smos_tools.data_types.aux_distan_datatype import datatype, flag_datatype
 from smos_tools.logger.logging_config import logging_config
 
 
@@ -35,6 +35,46 @@ def read_aux_distan(filename):
     return data
 
 
+def unpack_flag(distan_flags):
+    """
+    Unpacks the distan flags into a numpy structured array, including blanks
+
+    :param distan_flags: Flag component of the numpy structured data array for AUX_DISTAN
+    :return: a numpy structured array
+    """
+
+    # Make the empty array with the right dtype for the data
+
+    unpacked_flags = np.empty((len(distan_flags)), dtype=flag_datatype)
+
+    # unpack from Least Significant Bit
+
+    for position in range(0, len(flag_datatype)):
+        unpacked_flags[flag_datatype[position][0]] = (distan_flags >> position) & 1
+
+    return unpacked_flags
+
+
+def read_gridpoint_to_is_sea(filename):
+
+    # Read all data
+    data = read_aux_distan(filename)
+
+    # Unpack the flags into a matching length array
+    flags = unpack_flag(data['Flag'])
+
+    # Make a dataframe to manipulate these
+    dataframe = pd.DataFrame({'Grid_Point_ID': data['Grid_Point_ID'],
+        'Fg_Land_Sea_Coast1_tot': flags['Fg_Land_Sea_Coast1_tot'],
+        'Fg_Land_Sea_Coast2_tot': flags['Fg_Land_Sea_Coast2_tot']})
+
+    # Replace flag columns with a single column to signify sea. Is land when both flags are false, sea when either flag is true
+    dataframe['Is_Sea'] = dataframe.apply(lambda row: (row['Fg_Land_Sea_Coast1_tot'] or row['Fg_Land_Sea_Coast2_tot']), axis=1)
+    dataframe.drop(columns=['Fg_Land_Sea_Coast1_tot', 'Fg_Land_Sea_Coast2_tot'], inplace=True)
+
+    return dataframe
+
+
 if __name__ == '__main__':
 
-    dataframes = read_aux_distan("/home/smos/builds/v6.71/smos/products/AUX_/DISTAN/SM_OPER_AUX_DISTAN_20050101T000000_20500101T000000_001_011_3/SM_OPER_AUX_DISTAN_20050101T000000_20500101T000000_001_011_3.DBL")
+    data = read_gridpoint_to_is_sea("/home/smos/builds/v6.71/smos/products/AUX_/DISTAN/SM_OPER_AUX_DISTAN_20050101T000000_20500101T000000_001_011_3/SM_OPER_AUX_DISTAN_20050101T000000_20500101T000000_001_011_3.DBL")
